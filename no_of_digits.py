4c45cb1399ba8a7a5211adab3b7261fbcfc7c8d7
Divyanshu Khandelwal <khandelwalprinci1@gmail.com>
2024-01-16 23:55:56 +0530

    no of digits

---
+++ no_of_digits.py
@@ -0,0 +1,16 @@
+def count_digits(number):
+    num_digits = 0
+    abs_number = abs(number)  # handeling negative numbers
+
+    # Count digits using a loop
+    while abs_number > 0:
+        abs_number //= 10
+        num_digits += 1
+
+    return num_digits
+
+# Example usage:
+number_to_check = 12345
+result = count_digits(number_to_check)
+
+print(f"The number of digits in {number_to_check} is: {result}")
